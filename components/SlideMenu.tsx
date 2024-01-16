8886c00272c03a7a50801da1c0ee6e3bde03e7b2
AthenasCode <emoore5203@gmail.com>
2024-01-16 17:48:03 +0800

    added some sort of gesture logic...

--- components/SlideMenu.tsx
+++ components/SlideMenu.tsx
@@ -16,25 +16,38 @@ interface SlideMenuProps {
 
 export default function SlideMenu({ statusBarHeight }: SlideMenuProps) {
   const pressed = useSharedValue(false);
-  const width = useSharedValue(90);
+  const width = useSharedValue(200);
+  const openMenu = useSharedValue(false);
 
   const pan = Gesture.Pan()
     .onBegin(() => {
       pressed.value = true;
     })
     .onChange((event) => {
-      width.value = 50;
+      if (event.translationX > 0) {
+        openMenu.value = true;
+      } else if (event.translationX < 0) {
+        openMenu.value = false;
+      }
     })
     .onFinalize(() => {
-      width.value = 90;
+      pressed.value = false;
     });
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: withSpring(openMenu.value ? width.value : 0) }],
+  }));
 
   return (
     <GestureDetector gesture={pan}>
       <Animated.View
-        style={[styles.slideMenu, { width: width, top: statusBarHeight }]}
+        style={[
+          styles.slideMenu,
+          { width: width, top: statusBarHeight },
+          animatedStyles,
+        ]}
       >
-        <Text>Test</Text>
+        <Text>Menu content</Text>
       </Animated.View>
     </GestureDetector>
   );
@@ -43,7 +56,7 @@ export default function SlideMenu({ statusBarHeight }: SlideMenuProps) {
 const styles = StyleSheet.create({
   slideMenu: {
     position: "absolute",
-    left: 0,
+    left: -150,
     backgroundColor: "violet",
     height: "100%",
     zIndex: 2,
